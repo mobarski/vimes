0b973a6582de00a2c3d71b748b4504a415f10772
Maciej Obarski <mobarski@gmail.com>
2022-12-31 11:23:02 +0100

    direct threading WIP

--- src/c/v1_vm_direct.c
+++ src/c/v1_vm_direct.c
@@ -20,6 +20,7 @@ int run(int* code, int n, int mem_size, int code_size) {
 	int t; // temporary value (ie for top-of-stack)
 	char* args[] = {"P0","P1","P2","P3","P4","P5","P6","P7"};
 	
+	// rewrite code as instruction pointers (prog)
 	void **prog = calloc(code_size, sizeof(void*));
 	for (int i=0; i<code_size;) {
 		int op = code[i];
@@ -61,88 +62,53 @@ int run(int* code, int n, int mem_size, int code_size) {
 		}
 	}
 	
-	for (;;) {
-		if ((n>0) && (ic >= n)) break;
+	#define NEXT ic++; goto *prog[ip]
+	#define a code[ip+1]
+	{
+		NEXT;
 
-		int op = code[ip];
-		int a = code[ip+1];
-
-		switch (op) {
-			case LIT: goto _LIT;
-			case LOD: goto _LOD;
-			case STO: goto _STO;
-			case INT: goto _INT;
-			case JMP: goto _JMP;
-			case JZ:  goto _JZ;
-			case JNZ: goto _JNZ;
-			case INC: goto _INC;
-			case CAL: goto _CAL;
-			case OPR:
-				ip+=2;
-				switch (a) {
-					case ADD: goto _ADD;
-					case SUB: goto _SUB;
-					case MUL: goto _MUL;
-					case DIV: goto _DIV;
-					case MOD: goto _MOD;
-					case LT:  goto _LT;
-					case GT:  goto _GT;
-					case EQ:  goto _EQ;
-					case LE:  goto _LE;
-					case GE:  goto _GE;
-					case NE:  goto _NE;
-					case HLT: goto _HLT;
-					case RET: goto _RET;
-				}
-			case EXT:
-				ip+=2;
-				switch (a) {
-					case DOT: goto _DOT;
-					case AST: goto _AST;
-					case ARG: goto _ARG;
-				}
-		}
-
-		_LIT: mem[sp++]=a;           ip+=2;                        goto _NEXT;
-		_LOD: mem[sp++]=mem[fp-a];   ip+=2;                        goto _NEXT;
-		_STO: mem[fp-a]=mem[--sp];   ip+=2;                        goto _NEXT;
-		_INT: rp-=a;                 ip+=2;                        goto _NEXT;
-		_JMP: ip+=a;                                               goto _NEXT;
-		_JZ:  ip+=(mem[--sp]==0) ? a:2;                            goto _NEXT;
-		_JNZ: ip+=(mem[--sp]!=0) ? a:2;                            goto _NEXT;
-		_INC: mem[fp-a]+=code[ip+2]; ip+=3;                        goto _NEXT;
-		_CAL: mem[rp]=fp; mem[rp-1]=ip+2; ip=a; rp-=2; fp=rp;      goto _NEXT;
+		_LIT: mem[sp++]=a;           ip+=2;                        NEXT;
+		_LOD: mem[sp++]=mem[fp-a];   ip+=2;                        NEXT;
+		_STO: mem[fp-a]=mem[--sp];   ip+=2;                        NEXT;
+		_INT: rp-=a;                 ip+=2;                        NEXT;
+		_JMP: ip+=a;                                               NEXT;
+		_JZ:  ip+=(mem[--sp]==0) ? a:2;                            NEXT;
+		_JNZ: ip+=(mem[--sp]!=0) ? a:2;                            NEXT;
+		_INC: mem[fp-a]+=code[ip+2]; ip+=3;                        NEXT;
+		_CAL: mem[rp]=fp; mem[rp-1]=ip+2; ip=a; rp-=2; fp=rp;      NEXT;
+		
 		// OPR
-		_ADD: sp--; mem[sp-1] += mem[sp]; ip+=2;                   goto _NEXT;
-		_SUB: sp--; mem[sp-1] -= mem[sp]; ip+=2;                   goto _NEXT;
-		_MUL: sp--; mem[sp-1] *= mem[sp]; ip+=2;                   goto _NEXT;
-		_DIV: sp--; mem[sp-1] /= mem[sp]; ip+=2;                   goto _NEXT;
-		_MOD: sp--; mem[sp-1] %= mem[sp]; ip+=2;                   goto _NEXT;
-		_LT:  sp--; mem[sp-1] = mem[sp-1]  < mem[sp] ? 1:0; ip+=2; goto _NEXT;
-		_GT:  sp--; mem[sp-1] = mem[sp-1]  > mem[sp] ? 1:0; ip+=2; goto _NEXT;
-		_EQ:  sp--; mem[sp-1] = mem[sp-1] == mem[sp] ? 1:0; ip+=2; goto _NEXT;
-		_NE:  sp--; mem[sp-1] = mem[sp-1] != mem[sp] ? 1:0; ip+=2; goto _NEXT;
-		_LE:  sp--; mem[sp-1] = mem[sp-1] <= mem[sp] ? 1:0; ip+=2; goto _NEXT;
-		_GE:  sp--; mem[sp-1] = mem[sp-1] >= mem[sp] ? 1:0; ip+=2; goto _NEXT;
+		_ADD: sp--; mem[sp-1] += mem[sp]; ip+=2;                   NEXT;
+		_SUB: sp--; mem[sp-1] -= mem[sp]; ip+=2;                   NEXT;
+		_MUL: sp--; mem[sp-1] *= mem[sp]; ip+=2;                   NEXT;
+		_DIV: sp--; mem[sp-1] /= mem[sp]; ip+=2;                   NEXT;
+		_MOD: sp--; mem[sp-1] %= mem[sp]; ip+=2;                   NEXT;
+		_LT:  sp--; mem[sp-1] = mem[sp-1]  < mem[sp] ? 1:0; ip+=2; NEXT;
+		_GT:  sp--; mem[sp-1] = mem[sp-1]  > mem[sp] ? 1:0; ip+=2; NEXT;
+		_EQ:  sp--; mem[sp-1] = mem[sp-1] == mem[sp] ? 1:0; ip+=2; NEXT;
+		_NE:  sp--; mem[sp-1] = mem[sp-1] != mem[sp] ? 1:0; ip+=2; NEXT;
+		_LE:  sp--; mem[sp-1] = mem[sp-1] <= mem[sp] ? 1:0; ip+=2; NEXT;
+		_GE:  sp--; mem[sp-1] = mem[sp-1] >= mem[sp] ? 1:0; ip+=2; NEXT;
 		_HLT: ic++; goto _STOP;
-		_RET: ip=mem[fp+1]; rp=fp+2; fp=mem[fp+2];                 goto _NEXT;
+		_RET: ip=mem[fp+1]; rp=fp+2; fp=mem[fp+2];                 NEXT;
+		
 		// EXT
 		_ARG:
 			t = mem[sp-1];
 			mem[sp-1] = ((t>=0)&&(t<=7)) ? atoi(getenv(args[t])) : 0;
 			//printf("DEBUG: ARG %d %s = %d\n", t, args[t], mem[sp-1]); // XXX
 			ip+=2;
-			                                                  goto _NEXT;
-		_DOT: printf("%d\n",mem[sp-1]); ip+=2;                goto _NEXT;
+			                                                  NEXT;
+		_DOT: printf("%d\n",mem[sp-1]); ip+=2;                NEXT;
 		_AST: printf("AST not implemented\n"); goto _STOP; // TODO
-		_NEXT:
-		ic++;
+		
 	}
 	_STOP:
 	end = clock();
 	double dt_ms = 1000*((double)(end-start)) / CLOCKS_PER_SEC;
 	printf("STATUS: ip %d sp %d rp %d fp %d ic %lld dt %0.0f ms tos %d\n",ip,sp,rp,fp,ic,dt_ms,mem[sp-1]);
 	free(mem);
+	free(prog);
 	return ic;
 }
 
